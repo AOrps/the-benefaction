623770cb14cc1af700c06dbfd2af7b801667f071
AOrps <aorbeandrews@gmail.com>
2023-04-06 19:45:10 -0400

    buffer on clock-real-time.sh

--- clock-real-time.sh
+++ clock-real-time.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 #
 # Runs the clock ~(every second) and displays it using figlet
-#
+# 
 
 
-for i in {1..10000}; do clear && date "+%H : %M : %S" | figlet -c && sleep 1; done
+#for i in {1..10000}; do clear && date "+%H : %M : %S" | figlet -c && sleep 1; done
+for i in {1..10000}; do clear && echo -e "\n\n" && date "+%I : %M : %S" | figlet -c && sleep 1; done
+
+# the `echo -e '\n\n'` is to buffer the text
+
+: '
+tput cols - Get Columns
+tput lines - Get Rows
+'
+
