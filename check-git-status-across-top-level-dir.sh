ffffb9cd037de51ca481e73640ad1c0ef938aa6d
AOrps <aorbeandrews@gmail.com>
2024-07-06 21:28:11 -0400

    added check-git-status-across-top-level-dir.sh script edge case
    Signed-off-by: AOrps <aorbeandrews@gmail.com>

--- check-git-status-across-top-level-dir.sh
+++ check-git-status-across-top-level-dir.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-for d in `find . -maxdepth 1 -type d | tail -n +2`; do echo $d && cd $d && git status && echo -e "===================\n" && cd .. ; done
+# for d in `find . -maxdepth 1 -type d | tail -n +2`; do echo $d && cd $d && git status && echo -e "===================\n" && cd .. ; done
+
+
+# More complete solution that doesn't get caught if a file found isn't a git repo
+for d in `find . -name '.git' -type d | rev | cut -d'/' -f1 --complement | rev`; do echo $d && cd $d && git status && echo -e "===================\n" && cd .. ; done
+
